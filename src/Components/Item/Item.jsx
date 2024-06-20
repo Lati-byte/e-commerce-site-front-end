959edb91d7a5d20fa2c84c9b99472a26a6bc1cfb
Lati-byte <harmony.rams@gmail.com>
2024-06-20 15:37:14 +0200

    React frontend

---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import './Item.css'
+
+const Item = (props) => {
+    return (
+        <div className="item">
+            <img src={props.image} alt="" />
+            <p>{props.name}</p>
+            <div className="item-prices">
+                <div className="item-price-new">
+                    R{props.new_price}
+                </div>
+                <div className="item-price-old">
+                    R{props.old_price}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+// Define PropTypes for Item component
+Item.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    new_price: PropTypes.number.isRequired,
+    old_price: PropTypes.number.isRequired,
+  };
+
+export default Item
