3fabbafa402f661170e0489d7b4f9cf753917399
Lati-byte <harmony.rams@gmail.com>
2024-06-23 19:18:54 +0200

    added breadcum components and images to the product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+    const { all_products } = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_products.find((e) => e.id === Number(productId));
+
+    if (!product) {
+        return <div>Product not found</div>;
+    }
+
     return (
         <div>
-
+            <Breadcrum product={product} />
+            <ProductDisplay product={product}/>
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
